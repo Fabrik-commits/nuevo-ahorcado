04978eb29842f848c19835d44ff898c8b1f6c41a
Fabrik-commits <fabriknapo@gmail.com>
2026-07-07 15:23:40 -0300

    GREEN: Ahorcado.adivinar() descuenta vida al fallar

--- src/domain/Ahorcado.ts
+++ src/domain/Ahorcado.ts
@@ -2,13 +2,19 @@ export class Ahorcado {
   private readonly palabra: string;
   private readonly vidasIniciales: number = 6;
   private readonly letrasAdivinadas: string[] = [];
+  private vidasRestantes = this.vidasIniciales;
 
   constructor(palabra: string) {
     this.palabra = palabra.toUpperCase();
   }
 
   adivinar(letra: string): void {
-    this.letrasAdivinadas.push(letra.toUpperCase());
+    const letraNormalizada = letra.toUpperCase();
+    this.letrasAdivinadas.push(letraNormalizada);
+
+    if (!this.palabra.includes(letraNormalizada)) {
+      this.vidasRestantes -= 1;
+    }
   }
 
   palabraEnmascarada(): string {
@@ -18,7 +24,7 @@ export class Ahorcado {
       .join(" ");
   }
 
-    vidas(): number {
-    return this.vidasIniciales;
+  vidas(): number {
+    return this.vidasRestantes;
   }
 }
