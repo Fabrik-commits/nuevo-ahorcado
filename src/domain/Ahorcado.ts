d8987b81e6042cd265b3c63f381343982fc41682
Fabrik-commits <fabriknapo@gmail.com>
2026-07-07 00:43:33 -0300

    GREEN: Ahorcado.adivinar() revela todas las ocurrencias de la letra

--- src/domain/Ahorcado.ts
+++ src/domain/Ahorcado.ts
@@ -1,15 +1,20 @@
 export class Ahorcado {
   private readonly palabra: string;
   private readonly vidasIniciales: number = 6;
+  private readonly letrasAdivinadas: string[] = [];
 
   constructor(palabra: string) {
     this.palabra = palabra;
   }
 
+  adivinar(letra: string): void {
+    this.letrasAdivinadas.push(letra);
+  }
+
   palabraEnmascarada(): string {
     return this.palabra
       .split("")
-      .map(() => "_")
+      .map((c) => (this.letrasAdivinadas.includes(c) ? c : "_"))
       .join(" ");
   }
 
